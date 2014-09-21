1c25f0fdecfef22e6cbf6aad8ecab2e2331b60f0
Randy Ibarrola <randy.ibarrola@gmail.com>
2014-09-21 00:00:12 -0500

    mapa

--- templates/newdesing/js/home.js
+++ templates/newdesing/js/home.js
@@ -78,6 +78,7 @@ var Home = function() {
             $('#contact-map').html(App.imgLoading);
 
             if(typeof google != "undefined") {
+                $('#contact-map').empty();
                 var myLatlng = new google.maps.LatLng($('input[name=lat]').val(), $('input[name=lon]').val());
                 var icon = '/templates/images/condominium.png';
 
