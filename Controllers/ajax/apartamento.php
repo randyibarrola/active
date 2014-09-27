4da48d3a291a4828f4438509daea859c7d75446c
Randy Ibarrola <randy.ibarrola@gmail.com>
2014-09-26 21:30:38 -0500

    test

--- Controllers/ajax/apartamento.php
+++ Controllers/ajax/apartamento.php
@@ -557,11 +557,11 @@ if ($hotel) {
             $precio_seleccionado = convertFromMonedaToMoneda($precio, $hotel->moneda->codigo, $money);
             $precio_moneda_seleccionada = $currency . $precio_seleccionado;
             $aparts[$k]['precio_moneda_seleccionada'] = $precio_moneda_seleccionada;
-            $aparts[$k]['precio_moneda_seleccionada_especulado'] = $currency . ($precio_seleccionado + ( ( rand(20,50) * 100 ) / $precio_seleccionado ));
+            $aparts[$k]['precio_moneda_seleccionada_especulado'] = $currency . (  $precio_seleccionado + ( ( rand(20,50) * $precio_seleccionado ) / 100 ));
             
             var_dump($precio_seleccionado);
             var_dump($precio_moneda_seleccionada);
-            var_dump($currency . ($precio_seleccionado + ( ( rand(20,50) * 100 ) / $precio_seleccionado )));
+            var_dump($currency . number_format ( ($precio_seleccionado +  ( rand(20,50) * 100 ) / $precio_seleccionado ), 2,',' ,'.')  );
             var_dump($aparts[$k]);
             die;
             
